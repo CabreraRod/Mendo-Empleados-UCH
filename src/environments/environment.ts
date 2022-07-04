35393daef82811393b87fa81309d4c2029fcbe9f
CabreraRod <rodricabrera98@gmail.com>
2022-07-03 22:39:04 -0300

    upload de proyecto para parcial 2 de programacion 5

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCNy0NLrC5PQxwuRwPTQUGPdf9bcBSMpac",
+  authDomain: "empleados-eb0b4.firebaseapp.com",
+  projectId: "empleados-eb0b4",
+  storageBucket: "empleados-eb0b4.appspot.com",
+  messagingSenderId: "39893332572",
+  appId: "1:39893332572:web:297e122381887630a7ec99"
+  }
+};
+
+
